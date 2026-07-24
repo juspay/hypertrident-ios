965c09dbf45ec65f22ec9054c9bbdf9dadf15d4d
Balaganesh S <balaganesh@juspay.in>
2026-07-24 16:17:23 +0530

    Updated Package for version 2.2.8.4

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperTrident",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.3/HyperTrident.zip",
-            checksum: "42aa4cadbc14319f1af2eebf975f236f3e63ed2ec9ec99d2363f284b537eec30"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.4/HyperTrident.zip",
+            checksum: "523af510321cdcb1360f0f67549c4016903f119a3a5f5d1ab1ccb31f768a95e5"
         ),
         .target(
             name: "HyperTridentDependencies",
